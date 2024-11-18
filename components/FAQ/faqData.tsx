489a3675f3e871630d59948fc47ddfdc87180ed7
ROHAN SINGH <digitalsolutionindore@gmail.com>
2024-11-18 11:35:40 +0530

    first commit

---
+++ components/FAQ/faqData.tsx
@@ -0,0 +1,21 @@
+import { FAQ } from "@/types/faq";
+
+const faqData: FAQ[] = [
+  {
+    id: 1,
+    quest: "How can I contact Harmstech for support?",
+    ans: "You can reach our support team via email at support@harmastech.com or by calling us at +91-6269957381. We're here to assist you with any inquiries or assistance you may need.",
+  },
+  {
+    id: 2,
+    quest: "What payment methods do you accept for purchasing Harmstech's software?",
+    ans: "We accept various payment methods, including major credit cards, debit cards, bank transfers, and digital wallets.",
+  },
+  {
+    id: 3,
+    quest: "How often does Harmstech provide software updates?",
+    ans: "At Harmstech, we strive to continuously improve our software and provide regular updates to our users. Updates are released periodically to enhance functionality, security, and performance.",
+  },
+];
+
+export default faqData;
---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Full API Integration",
+    description:
+      "Experience the complete transformation of your trading journey with our comprehensive Full API Integration service. Seamlessly link your trading systems with top platforms, guaranteeing instant data access, rapid execution, and a smooth trading expedition.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Back Tested Strategies",
+    description:
+      "Empower your trading decisions with confidence through our Back Tested Strategies service. We meticulously examine historical data to validate and refine trading strategies, offering valuable insights into their performance across diverse market conditions.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Risk Management",
+    description:
+      "Protect your investments with our dedicated Risk Management service. We implement robust measures to mitigate risks, ensuring a balanced trading approach that minimizes potential losses and enhances the overall security of your portfolio.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Multiple Strategies",
+    description:
+      "Diversify your trading portfolio effortlessly with our Multiple Strategies service. Select from a variety of proven algorithms, each tailored to address different market conditions. This flexibility enables you to adjust and refine your strategies for various trading scenarios, maximizing your potential for success.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "FULL Automation",
+    description:
+      "Unlock the convenience of hands-free trading with our FULL Automation service. Allow our advanced algorithms to execute trades on your behalf, ensuring precision and efficiency in every market move. Bid farewell to manual intervention and embrace the simplicity of automated trading.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Regular Free Updates",
+    description:
+      "Stay ahead of the curve with our Regular Updates service. We're dedicated to keeping you informed and equipped with the latest advancements in algorithmic trading. Enjoy continuous improvements, feature enhancements, and market insights to ensure your strategies remain cutting-edge.",
+  },
+];
+
+export default featuresData;
