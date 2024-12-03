0911b781810333eef733eab31620d79ab9f141b5
Pushpendraraj7879 <pushpendraraj7879@gmail.com>
2024-12-03 20:10:30 +0530

    Update page.tsx

--- components/Whatsapp/page.tsx
+++ components/Whatsapp/page.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { IoLogoWhatsapp } from "react-icons/io";
+const WpButton = () => {
+  return (
+    <a
+      href="https://wa.me/+916269957381"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="flex items-center max-w-28 p-2 rounded-xl cursor-pointer fixed bottom-6 right-6 z-[3500] bg-white border shadow-xl">
+        <p className="mr-2 text-xl text-green-600 md:text-3xl">
+          {<IoLogoWhatsapp />}
+        </p>
+        <h1 className="text-gray-700">
+          <span className="font-semibold text-black">Ask</span>
+        </h1>
+      </div>
+    </a>
+  );
+};
 
+export default WpButton;
